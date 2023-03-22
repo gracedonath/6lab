e030e73ea370e337c1c02a5662ca9d4a3959e80c
gracedonath <gracedonath@ufl.edu>
2023-03-22 14:36:44 -0400

    1st correction

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # grace donath
+
+def encoder(password):
+    new_pass = ''
+    for num in password:
+        new_num = int(num) + 3
+        new_pass += str(new_num)
+    return new_pass
+
+def main():
+    password = input('Number?')
+    print(encoder(password))
+
+if __name__ == "__main__":
+    main()
